e86628c7727da27a3c24935aa35802a1ed902db3
danilsdf <48866421+danilsdf@users.noreply.github.com>
2019-11-22 13:57:54 +0200

    g

---
+++ Tetris/Tetris/BaseTetris.cs
@@ -0,0 +1,384 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Tetris
+{
+    public class BaseTetris
+    {
+        public static int[,] A = new int[4, 4] { {0, 0, 0, 0 },
+                                                 {1, 1, 1, 1 },
+                                                 {0, 0, 0, 0 },
+                                                 {0, 0, 0, 0 }};
+        public static int[,] B = new int[2, 2] { {1, 1 },
+                                                 {1, 1 }};
+        public static int[,] C = new int[3, 3] { {0, 1, 0 },
+                                                 {1, 1, 1 },
+                                                 {0, 0, 0 }};
+        public static int[,] D = new int[3, 3] { {0, 1, 1 },
+                                                 {1, 1, 0 },
+                                                 {0, 0, 0 }};
+        public static int[,] F = new int[3, 3] { {1, 1, 0 },
+                                                 {0, 1, 1 },
+                                                 {0, 0, 0 }};
+        public static int[,] E = new int[3, 3] { {1, 0, 0 },
+                                                 {1, 1, 1 },
+                                                 {0, 0, 0 }};
+        public static int[,] K = new int[3, 3] { {0, 0, 1 },
+                                                 {1, 1, 1 },
+                                                 {0, 0, 0 }};
+        public static List<int[,]> tetrisbase = new List<int[,]>() { A, B, C, D, F, E, K };
+        public List<int[]> loc = new List<int[]>();
+        static int[,] forma;
+        private bool isErect = false;
+
+        public BaseTetris()
+        {
+            Random rnd = new Random();
+            Program.NumberOfForm = rnd.Next(0, 7);
+            forma = tetrisbase[Program.NumberOfForm];
+            for (int i = 20; i < 35; ++i)
+            {
+                for (int j = 4; j < 10; ++j)
+                {
+                    Console.SetCursorPosition(i, j);
+                    Console.Write("  ");
+                }
+            }
+            Program.Map();
+
+            for (int i = 0; i < forma.GetLength(0); i++)
+            {
+                for (int f = 0; f < forma.GetLength(1); f++)
+                {
+                    if (forma[i, f] == 1)
+                    {
+
+                        Console.SetCursorPosition(((10 - forma.GetLength(1)) / 2 + f) * 2 + 20, i + 5);
+                        Console.Write(Program.kybik);
+                    }
+                }
+            }
+        }
+        public void Smena()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                for (int f = 0; f < 10; f++)
+                {
+                    if (Program.pole[i, f] != 2) Program.pole[i, f] = 0;
+                }
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                Program.pole[loc[i][0], loc[i][1]] = 1;
+            }
+            Program.Paint();
+        }
+        public void Fall()
+        {
+
+            if (cheTo_c_nizy())
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    Program.pole[loc[i][0], loc[i][1]] = 2;
+                }
+                Program.Padaet = true;
+            }
+
+            else
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    loc[i][0] += 1;
+                }
+                Smena();
+            }
+        }
+        public void Spawn()
+        {
+            for (int i = 0; i < forma.GetLength(0); i++)
+            {
+                for (int f = 0; f < forma.GetLength(1); f++)
+                {
+                    if (forma[i, f] == 1)
+                    {
+                        loc.Add(new int[] { i, (10 - forma.GetLength(1)) / 2 + f });
+                    }
+                }
+            }
+            Smena();
+        }
+        public void Rotate()
+        {
+            List<int[]> tetloc = new List<int[]>();
+            for (int i = 0; i < forma.GetLength(0); i++)
+            {
+                for (int j = 0; j < forma.GetLength(1); j++)
+                {
+                    if (forma[i, j] == 1)
+                    {
+                        tetloc.Add(new int[] { i, (10 - forma.GetLength(1)) / 2 + j });
+                    }
+                }
+            }
+
+            if (Program.NumberOfForm1 == 0)
+            {
+                if (isErect == false)
+                {
+                    for (int i = 0; i < loc.Count; i++)
+                    {
+                        tetloc[i] = TransformMatrix(loc[i], loc[2], "Clockwise");
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < loc.Count; i++)
+                    {
+                        tetloc[i] = TransformMatrix(loc[i], loc[2], "Counterclockwise");
+                    }
+                }
+            }
+
+            else if (Program.NumberOfForm1 == 3)
+            {
+                for (int i = 0; i < loc.Count; i++)
+                {
+                    tetloc[i] = TransformMatrix(loc[i], loc[3], "Clockwise");
+                }
+            }
+
+            else if (Program.NumberOfForm1 == 1) return;
+            else
+            {
+                for (int i = 0; i < loc.Count; i++)
+                {
+                    tetloc[i] = TransformMatrix(loc[i], loc[2], "Clockwise");
+                }
+            }
+
+
+            for (int count = 0; Meshaet_cleva(tetloc) != false | Meshaet_cprava(tetloc) != false | Meshaet_cverxy(tetloc) != false; count++)
+            {
+                if (Meshaet_cleva(tetloc) == true)
+                {
+                    for (int i = 0; i < loc.Count; i++)
+                    {
+                        tetloc[i][1] += 1;
+                    }
+                }
+
+                if (Meshaet_cprava(tetloc) == true)
+                {
+                    for (int i = 0; i < loc.Count; i++)
+                    {
+                        tetloc[i][1] -= 1;
+                    }
+                }
+                if (Meshaet_cverxy(tetloc) == true)
+                {
+                    for (int i = 0; i < loc.Count; i++)
+                    {
+                        tetloc[i][0] -= 1;
+                    }
+                }
+                if (count == 3)
+                {
+                    return;
+                }
+            }
+
+            loc = tetloc;
+        }
+        public int[] TransformMatrix(int[] coord, int[] axis, string direction)
+        {
+            int[] pcoord = { coord[0] - axis[0], coord[1] - axis[1] };
+            if (direction == "Counterclockwise")
+            {
+                pcoord = new int[] { -pcoord[1], pcoord[0] };
+            }
+            else if (direction == "Clockwise")
+            {
+                pcoord = new int[] { pcoord[1], -pcoord[0] };
+            }
+
+            return new int[] { pcoord[0] + axis[0], pcoord[1] + axis[1] };
+        }
+        public bool? Meshaet_cverxy(List<int[]> loc)
+        {
+            List<int> cordinataY = new List<int>();
+            for (int i = 0; i < 4; i++)
+            {
+                cordinataY.Add(loc[i][0]);
+                if (loc[i][0] >= 19)
+                    return true;
+                if (loc[i][0] < 0 || loc[i][1] < 0 || loc[i][1] > 9)
+                    return null;
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                if (cordinataY.Max() - cordinataY.Min() == 3)
+                {
+                    if (cordinataY.Max() == loc[i][0] || cordinataY.Max() - 1 == loc[i][0])
+                    {
+                        if (Program.pole[loc[i][0], loc[i][1]] == 2)
+                        {
+                            return true;
+                        }
+                    }
+
+                }
+                else
+                {
+                    if (cordinataY.Max() == loc[i][0])
+                    {
+                        if (Program.pole[loc[i][0], loc[i][1]] == 2)
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+
+            return false;
+        }
+        public bool? Meshaet_cleva(List<int[]> loc)
+        {
+            List<int> cordinataX = new List<int>();
+            for (int i = 0; i < 4; i++)
+            {
+                cordinataX.Add(loc[i][1]);
+                if (loc[i][1] < 0)
+                {
+                    return true;
+                }
+                if (loc[i][1] > 9)
+                {
+                    return false;
+                }
+                if (loc[i][0] >= 19 || loc[i][0] < 0)
+                    return null;
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                if (cordinataX.Max() - cordinataX.Min() == 3)
+                {
+                    if (cordinataX.Min() == loc[i][1] | cordinataX.Min() + 1 == loc[i][1])
+                    {
+                        if (Program.pole[loc[i][0], loc[i][1]] == 2)
+                        {
+                            return true;
+                        }
+                    }
+
+                }
+                else
+                {
+                    if (cordinataX.Min() == loc[i][1])
+                    {
+                        if (Program.pole[loc[i][0], loc[i][1]] == 2)
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+        public bool? Meshaet_cprava(List<int[]> loc)
+        {
+            List<int> cordinataX = new List<int>();
+            for (int i = 0; i < 4; i++)
+            {
+                cordinataX.Add(loc[i][1]);
+                if (loc[i][1] > 9)
+                {
+                    return true;
+                }
+                if (loc[i][1] < 0)
+                {
+                    return false;
+                }
+                if (loc[i][0] >= 19 || loc[i][0] < 0)
+                    return null;
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                if (cordinataX.Max() - cordinataX.Min() == 3)
+                {
+                    if (cordinataX.Max() == loc[i][1] | cordinataX.Max() - 1 == loc[i][1])
+                    {
+                        if (Program.pole[loc[i][0], loc[i][1]] == 2)
+                        {
+                            return true;
+                        }
+                    }
+
+                }
+                else
+                {
+                    if (cordinataX.Max() == loc[i][1])
+                    {
+                        if (Program.pole[loc[i][0], loc[i][1]] == 2)
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+        public bool cheTo_c_nizy()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                if (loc[i][0] + 1 >= 19)
+                {
+                    return true;
+                }
+
+                if (loc[i][0] + 1 < 19)
+                {
+                    if (Program.pole[loc[i][0] + 1, loc[i][1]] == 2)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        public bool cheto_cprava()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                if (loc[i][1] == 9)
+                {
+                    return true;
+                }
+                else if (Program.pole[loc[i][0], loc[i][1] + 1] == 2)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public bool cheto_cleva()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                if (loc[i][1] == 0)
+                {
+                    return true;
+                }
+                else if (Program.pole[loc[i][0], loc[i][1] - 1] == 2)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
+
---
+++ Tetris/Tetris/Program.cs
@@ -0,0 +1,282 @@
+﻿using System;
+using System.Diagnostics;
+using System.Media;
+
+namespace Tetris
+{
+    class Program
+    {
+        public static Stopwatch timer = new Stopwatch();
+        public static Stopwatch dropTimer = new Stopwatch();
+        public static Stopwatch inputTimer = new Stopwatch();
+        public static int[,] pole = new int[20, 10];
+        public static string kybik = "■";
+        public static int dropTime, dropRate = 300;             //for levels
+        public static int linesCleared = 0, score = 0, level = 1;
+        public static int NumberOfForm1, NumberOfForm;
+        static BaseTetris form;
+        static BaseTetris nextform;
+        public static ConsoleKeyInfo key;
+        public static bool isKeyPressed = false;
+        public static bool Padaet = false;
+        public SoundPlayer Z = new SoundPlayer(@"tetris.wav");
+
+        static void Main()
+        {
+            SoundPlayer splayer = new SoundPlayer();
+            splayer.SoundLocation = Environment.CurrentDirectory + "\\01. Main Menu.wav";
+            splayer.PlayLooping();
+
+            Console.CursorVisible = false;
+            timer.Start();
+            dropTimer.Start();
+            long time = timer.ElapsedMilliseconds;
+            Map();
+            Console.SetCursorPosition(4, 10);
+            Console.WriteLine("Press any key!");
+            Console.ReadKey(true);
+            Console.Clear();
+            Map();
+            Console.SetCursorPosition(25, 1);
+            Console.WriteLine("Level " + level);
+            Console.SetCursorPosition(25, 2);
+            Console.WriteLine("Score " + score);
+            Console.SetCursorPosition(25, 3);
+            Console.WriteLine("LinesCleared " + linesCleared);
+
+            splayer.Stop();
+            splayer.SoundLocation = Environment.CurrentDirectory + "\\02. Game Theme.wav";
+            splayer.Play();
+
+
+            nextform = new BaseTetris();
+            NumberOfForm1 = NumberOfForm;
+            form = nextform;
+            form.Spawn();
+            nextform = new BaseTetris();
+            Perezapysk();
+
+            splayer.Stop();
+            splayer.SoundLocation = Environment.CurrentDirectory + "\\05. Results.wav";
+            splayer.Play();
+
+            Console.Clear();
+            Console.SetCursorPosition(5, 5);
+            Console.WriteLine("Game Over \n\t Replay? (Y/N)");
+            Console.SetCursorPosition(5, 7);
+            Console.WriteLine("Your score: " + score);
+            string input = Console.ReadLine();
+
+            if (input == "y" || input == "Y")
+            {
+                pole = new int[20, 10];
+                timer = new Stopwatch();
+                dropTimer = new Stopwatch();
+                inputTimer = new Stopwatch();
+                dropRate = 300;
+                Padaet = false;
+                isKeyPressed = false;
+                linesCleared = 0;
+                score = 0;
+                level = 1;
+                GC.Collect();
+                Console.Clear();
+                Main();
+            }
+            else return;
+        }
+        public static void Map()
+        {
+            Console.SetCursorPosition(0, 0);
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("#-");
+            }
+            for (int i = 0; i < 19; i++)
+            { Console.WriteLine("#"); }
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("#-");
+            }
+            for (int i = 1; i < 20; i++)
+            {
+                Console.SetCursorPosition(20, i);
+                Console.WriteLine("#");
+            }
+        }
+        public static void Paint()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                for (int f = 0; f < 10; f++)
+                {
+                    Console.SetCursorPosition(1 + 2 * f, i);
+
+                    if (pole[i, f] == 1 || pole[i, f] == 2)
+                    {
+                        Console.SetCursorPosition(1 + 2 * f, i);
+                        Console.Write(kybik);
+                    }
+                    else
+                    {
+
+                        Console.Write(" ");
+                    }
+                }
+            }
+        }
+        static void Perezapysk()
+        {
+            while (true)
+            {
+                dropTime = (int)dropTimer.ElapsedMilliseconds;
+                if (dropTime > dropRate)
+                {
+                    dropTime = 0;
+                    dropTimer.Restart();
+                    form.Fall();
+
+                }
+                if (Padaet == true)
+                {
+                    form = nextform;
+                    NumberOfForm1 = NumberOfForm;
+                    form.Spawn();
+                    nextform = new BaseTetris();
+                    Padaet = false;
+                }
+                for (int i = 0; i < 10; i++)
+                {
+                    if (pole[0, i] == 2)
+                        return;
+                }
+                Doings();
+                ClearBlock();
+            }
+        }
+        private static void ClearBlock()
+        {
+            int combo = 0;
+            for (int i = 0; i < 20; i++)
+            {
+                int j;
+                for (j = 0; j < 10; j++)
+                {
+                    if (pole[i, j] != 2)
+                        break;
+                }
+                if (j == 10)
+                {
+
+                    linesCleared++;
+                    combo++;
+                    for (j = 0; j < 10; j++)
+                    {
+                        pole[i, j] = 0;
+                    }
+                    int[,] newpole = new int[20, 10];
+                    for (int k = 1; k < i; k++)
+                    {
+                        for (int l = 0; l < 10; l++)
+                        {
+                            if (pole[k, l] == 2)
+                            { newpole[k + 1, l] = 2; }
+                            else
+                                newpole[k + 1, l] = 0;
+                        }
+                    }
+                    for (int k = 1; k < i; k++)
+                    {
+                        for (int l = 0; l < 10; l++)
+                        {
+                            pole[k, l] = 0;
+                        }
+                    }
+                    for (int k = 0; k < 20; k++)
+                        for (int l = 0; l < 10; l++)
+                            if (newpole[k, l] == 2)
+                                pole[k, l] = 2;
+                    Paint();
+                }
+            }
+            if (combo == 1)
+                score += 50 * level;
+            else if (combo == 2)
+                score += 100 * level;
+            else if (combo == 3)
+                score += 300 * level;
+            else if (combo > 3)
+                score += 300 * combo * level;
+
+            if (linesCleared < 5) level = 1;
+            else if (linesCleared < 10) level = 2;
+            else if (linesCleared < 15) level = 3;
+            else if (linesCleared < 25) level = 4;
+            else if (linesCleared < 35) level = 5;
+            else if (linesCleared < 50) level = 6;
+            else if (linesCleared < 70) level = 7;
+            else if (linesCleared < 90) level = 8;
+            else if (linesCleared < 110) level = 9;
+            else if (linesCleared < 150) level = 10;
+
+
+            if (combo > 0)
+            {
+                Console.SetCursorPosition(25, 1);
+                Console.WriteLine("Level " + level);
+                Console.SetCursorPosition(25, 2);
+                Console.WriteLine("Score " + score);
+                Console.SetCursorPosition(25, 3);
+                Console.WriteLine("LinesCleared " + linesCleared);
+            }
+
+            dropRate = 300 - 22 * level;
+
+        }
+        public static void Doings()
+        {
+            if (Console.KeyAvailable)
+            {
+                key = Console.ReadKey();
+                isKeyPressed = true;
+            }
+            else
+                isKeyPressed = false;
+
+            if (Program.key.Key == ConsoleKey.LeftArrow & !form.cheto_cleva() & isKeyPressed)
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    form.loc[i][1] -= 1;
+                }
+                form.Smena();
+            }
+            if (Program.key.Key == ConsoleKey.RightArrow & !form.cheto_cprava() & isKeyPressed)
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    form.loc[i][1] += 1;
+                }
+                form.Smena();
+            }
+            if (Program.key.Key == ConsoleKey.DownArrow & isKeyPressed)
+            {
+                form.Fall();
+
+            }
+            if (Program.key.Key == ConsoleKey.Spacebar & isKeyPressed)
+            {
+                form.Rotate();
+
+            }
+            if (Program.key.Key == ConsoleKey.UpArrow & !form.cheTo_c_nizy() & isKeyPressed)
+            {
+                while (form.cheTo_c_nizy() != true)
+                {
+                    form.Fall();
+                }
+            }
+
+        }
+    }
+}
